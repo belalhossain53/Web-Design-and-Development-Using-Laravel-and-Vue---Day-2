de64be746cdd8858946a9ed5e351c5ab26df01d7
Md. Belal Hossain <belalhossain53@gmail.com>
2024-05-08 00:58:18 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use App\classes\WelcomeMsg;
+use App\classes\Books;
+
+$welcomemsg = new WelcomeMsg();
+$welcomemsg->index();
+
+/*
+$book = new Books();
+$book->index();
+ */
+
+/*
+ echo "Welcome to Web Design and Development Using Laravel and Vue.";
+ */
+
+/*
+ function CMessage()
+{
+    echo "Welcome to Web Design and Development Using Laravel and Vue.";
+}
+
+CMessage();
+ */
+
+/*
+ class CMessage
+{
+    public $message;
+
+    public function messagef()
+    {
+        $this->message="Welcome to Web Design and Development Using Laravel and Vue.";
+        echo $this->message;
+    }
+}
+
+$cmessage = new CMessage();
+$cmessage->messagef();
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
